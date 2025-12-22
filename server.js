7fdbae9849cfb327b56a48c78c0b73698548023a
Dendup1234 <dendup091@gmail.com>
2025-12-22 17:25:22 +0600

    Added nodemon

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 //Imports
-import express from express;
-import dotenv from dotenv;
+import express from 'express';
+import dotenv from 'dotenv';
 
 //config
 dotenv.config();
@@ -10,9 +10,9 @@ app.use(express.json());
 const port = 3000;
 
 app.get('/', (request, response) => {
-	response.send('Hello world');
+	response.send('Hello world from the nodemon');
 })
 
 app.listen(port, () => {
-	console.log('Server is running');
+	console.log('Server is running on the nodemon');
 });

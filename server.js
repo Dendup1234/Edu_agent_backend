0ab52fb597ed2294f41c90fb66a829c97cbc77de
Dendup1234 <dendup091@gmail.com>
2025-12-22 17:15:53 +0600

    Folder structure created

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
-const express = require('express');
+//Imports
+import express from express;
+import dotenv from dotenv;
+
+//config
+dotenv.config();
+
 const app = express();
+app.use(express.json());
 const port = 3000;
 
 app.get('/', (request, response) => {

c4b78af4cccae62c324d52af43916faaec4faebc
Dendup1234 <dendup091@gmail.com>
2025-12-24 12:08:52 +0600

    Done with otp send,verify,resend

--- routes/user.auth.route.js
+++ routes/user.auth.route.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { login, register } from "../controllers/user.auth.controller.js";
+//import { login, register } from "../controllers/user.auth.controller.js";
 import { protect } from "../middlewares/auth.middleware.js";
-
+import { sendOrResendOtp, verifyOtp, login } from "../controllers/auth.controller.js"
 const router = express.Router();
 
+router.post("/send-otp", sendOrResendOtp);
+router.post("/verify-otp", verifyOtp);
 router.post("/login", login);
-router.post("/register", register);
 router.get("/profile", protect, (req, res) => {
 	res.json({
 		message: "Protected route",

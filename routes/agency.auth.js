40f7dd76542b8ba7d72c1ae2d6e3b330584fff03
Dendup1234 <dendup091@gmail.com>
2025-12-30 16:00:04 +0600

    Added get profile for agency

--- routes/agency.auth.js
+++ routes/agency.auth.js
@@ -1,27 +1,35 @@
 import express from "express";
 import { protect } from "../middlewares/auth.middleware.js";
-import { sendOtp,resendOtp, verifyOtp, login, sendPasswordResetOtp, verifyPasswordResetOtp, setNewPassword } from "../controllers/agency.auth.js"
-import Agency from "../models/agency.js"
+import {
+  sendOtp,
+  resendOtp,
+  verifyOtp,
+  login,
+  sendPasswordResetOtp,
+  verifyPasswordResetOtp,
+  setNewPassword,
+} from "../controllers/agency.auth.js";
+import Agency from "../models/agency.js";
 const router = express.Router();
 
 router.post("/send-otp", sendOtp);
 router.post("/resend-otp", resendOtp);
 router.post("/verify-otp", verifyOtp);
 router.post("/login", login);
-router.post("/password-reset/send-otp", sendPasswordResetOtp);   
+router.post("/password-reset/send-otp", sendPasswordResetOtp);
 router.post("/password-reset/verify-otp", verifyPasswordResetOtp);
-router.post("/password-reset/set-new", setNewPassword);   
-//profile
+router.post("/password-reset/set-new", setNewPassword);
+//profiles specific
 router.get("/profile", protect, async (req, res) => {
   try {
     const user_id = req.user.sub;
     //Hides password and return plain json format
-    const student = await Agency.findById(user_id).select("-password").lean();
-    if (!student) {
+    const agency = await Agency.findById(user_id).select("-password").lean();
+    if (!agency) {
       return res.status(404).json({ message: "User not found" });
     }
     return res.json({
-      profile: student,
+      profile: agency,
       tokenUser: { userId: user_id, email: req.user.email },
     });
   } catch (e) {
@@ -29,6 +37,19 @@ router.get("/profile", protect, async (req, res) => {
     return res.status(500).json({ message: "Server error" });
   }
 });
+//profiles all
+router.get("/", protect, async(req, res) => {
+  try {
+      const agency = await Agency.find().select("-password").lean();
+      return res.json({
+        count: agency.length,
+        agency,
+      });
+    } catch (e) {
+      console.log(e);
+      return res.status(500).json({ message: "Server error" });
+    }
+
+});
 
 export default router;
-

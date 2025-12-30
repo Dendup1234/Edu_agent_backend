c06982eadddf2f44f5b7d849b13161436cb227ae
Dendup1234 <dendup091@gmail.com>
2025-12-30 14:58:39 +0600

    Added a agency profile route

--- routes/student.auth.js
+++ routes/student.auth.js
@@ -12,17 +12,22 @@ import {
 import Student from "../models/student.js";
 const router = express.Router();
 
+//For authentication
 router.post("/send-otp", sendOtp);
 router.post("/resend-otp", resendOtp);
 router.post("/verify-otp", verifyOtp);
 router.post("/login", login);
-router.get("/profile", protect, async(req, res) => {
+router.post("/password-reset/send-otp", sendPasswordResetOtp);
+router.post("/password-reset/verify-otp", verifyPasswordResetOtp);
+router.post("/password-reset/set-new", setNewPassword);
+
+//Crud functionality in the profile
+//Specific user
+router.get("/profile", protect, async (req, res) => {
   try {
     const user_id = req.user.sub;
-	console.log(user_id)
-	//Hides password and return plain json format
-    const student = await Student.findById(user_id).select('-password').lean();
-    //User not found
+    //Hides password and return plain json format
+    const student = await Student.findById(user_id).select("-password").lean();
     if (!student) {
       return res.status(404).json({ message: "User not found" });
     }
@@ -35,8 +40,46 @@ router.get("/profile", protect, async(req, res) => {
     return res.status(500).json({ message: "Server error" });
   }
 });
-router.post("/password-reset/send-otp", sendPasswordResetOtp);
-router.post("/password-reset/verify-otp", verifyPasswordResetOtp);
-router.post("/password-reset/set-new", setNewPassword);
+// All user
+router.get("/", protect, async (req, res) => {
+  try {
+    const students = await Student.find().select("-password").lean();
+    return res.json({
+      count: students.length,
+      students,
+    });
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({ message: "Server error" });
+  }
+});
+
+//Update
+router.patch("/profile", protect, async (req, res) => {
+  try {
+    const userId = req.user.sub;
+    const update = req.body;
+    // forbidden fields to be updated
+    const forbidden = ["_id", "passoword"];
+    forbidden.forEach((field) => delete update[field]);
+    //Find by id and update
+    const updatedStudent = await Student.findByIdAndUpdate(userId, update, {
+      new: true,
+      runValidators: true,
+    })
+      .select("-password")
+      .lean();
+
+    if (!updatedStudent) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.json({
+      message: "Profile updated",
+      profile: updatedStudent,
+    });
+  } catch (e) {
+    return res.status(500).json({ message: "Server error" });
+  }
+});
 
 export default router;

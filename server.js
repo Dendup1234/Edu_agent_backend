11541931ac432c23cfede7eb9c7faaf7d215efd1
yumiko-senpai <damcheynorbu7739@gmail.com>
2025-12-31 12:00:04 +0600

    cors configuration

--- server.js
+++ server.js
@@ -13,12 +13,7 @@ dotenv.config();
 // app config
 const app = express();
 app.use(express.json());
-app.use(cors({
-  origin: "http://localhost:3000",
-  methods: ["GET", "POST", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true
-}));
+app.use(cors());
 
 //routes
 app.use("/api/v1/students", studentRoute);

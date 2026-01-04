e6362fe1abfb6f00152d44362626a88fc2cb40c5
Dendup1234 <dendup091@gmail.com>
2026-01-04 23:25:29 +0600

    Added a creating university api

--- models/university.js
+++ models/university.js
@@ -1,43 +1,34 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
-const universitySchema = new mongoose.Schema(
+const universitySchema = new Schema(
   {
     name: {
       type: String,
-      required: true,
-      trim: true,
-      unique: true,
     },
     logo: {
       type: String,
-      trim: true,
     },
     websiteURL: {
       type: String,
-      trim: true
     },
     country: {
       type: String,
-      required: true,
-      trim: true,
     },
     about: {
       type: String,
-      trim: true,
     },
-    mission:{
-        type: String,
-        trim: true
+    mission: {
+      type: String,
     },
-    status:{
-        type: String,
-        enum:["Active","Inactive"],
-        default: "Active"
-    }
+    status: {
+      type: String,
+      enum: ["Active", "Inactive"],
+      default: "Active",
+    },
   },
   {
-    timestamps: true, 
+    timestamps: true,
   }
 );
 
-export const University = mongoose.Schema('University',universitySchema);
+export default mongoose.model("University", universitySchema);
--- routes/agency.auth.js
+++ routes/agency.auth.js
@@ -10,6 +10,9 @@ import {
   setNewPassword,
 } from "../controllers/agency.auth.js";
 import Agency from "../models/agency.js";
+import University from "../models/university.js";
+
+// Router import
 const router = express.Router();
 
 router.post("/send-otp", sendOtp);
@@ -19,6 +22,7 @@ router.post("/login", login);
 router.post("/password-reset/send-otp", sendPasswordResetOtp);
 router.post("/password-reset/verify-otp", verifyPasswordResetOtp);
 router.post("/password-reset/set-new", setNewPassword);
+
 //profiles specific
 router.get("/profile", protect, async (req, res) => {
   try {
@@ -37,19 +41,6 @@ router.get("/profile", protect, async (req, res) => {
     return res.status(500).json({ message: "Server error" });
   }
 });
-//profiles all
-router.get("/", protect, async (req, res) => {
-  try {
-    const agency = await Agency.find().select("-password").lean();
-    return res.json({
-      count: agency.length,
-      agency,
-    });
-  } catch (e) {
-    console.log(e);
-    return res.status(500).json({ message: "Server error" });
-  }
-});
 
 //updating profile
 router.patch("/profile", protect, async (req, res) => {
@@ -59,7 +50,7 @@ router.patch("/profile", protect, async (req, res) => {
     // forbidden fields to be updated
     const forbidden = ["_id", "password"];
     forbidden.forEach((field) => delete update[field]);
-    //Find by id and update
+    //Find by id and updateeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJnb29nbGVJZCI6IjExMjM2NzE5NDE5MDUxOTczNTAyNCIsImlkIjoiNjk1N2YwOTM1NTgyNTdmNWZkZjljMGY5IiwiaWF0IjoxNzY3NDMwNjc2fQ.Gaph65C38IBZ0MIJIr5MP_DvqFS5WcZmK4NZaJFpeds
     const updatedAgency = await Agency.findByIdAndUpdate(userId, update, {
       new: true,
       runValidators: true,
@@ -75,6 +66,43 @@ router.patch("/profile", protect, async (req, res) => {
       profile: updatedAgency,
     });
   } catch (e) {
+    return res.status(500).json({ message: "Server error" });
+  }
+});
+
+// Creating a university
+router.post("/university", protect, async (req, res) => {
+  try {
+    const userId = req.user.sub;
+    const { name, logo, websiteURL, country, about, mission, status } =
+      req.body;
+    const university = await University.create({
+      name,
+      logo,
+      websiteURL,
+      country,
+      about,
+      mission,
+      status,
+    });
+    //Referencing the agency to the university
+    const agency = await Agency.findByIdAndUpdate(userId, {
+      $addToSet: { partnerUniversities: university._id },
+    });
+    if (!agency) {
+      return res.status(404).json({ message: "Unauthorized" });
+    }
+
+    return res.status(201).json({
+      message: "University created successfully",
+      university: university,
+      agency: {
+        agency_id: agency._id,
+        partnerUniversities: agency.partnerUniversities,
+      },
+    });
+  } catch (e) {
+    console.log(e);
     return res.status(500).json({ message: "Server error" });
   }
 });

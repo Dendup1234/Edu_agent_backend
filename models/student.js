e4c07799731bf60e8b64d2f18943f9c572ac1aaf
yumiko-senpai <damcheynorbu7739@gmail.com>
2026-01-11 18:06:17 +0600

    UPDATE: student model

--- models/student.js
+++ models/student.js
@@ -89,10 +89,6 @@ const StudentSchema = new Schema(
       type: String,
       enum: ["active", "inactive"],
       default: "active"
-    },
-
-    profilePicture: {
-      type: String
     }
   },
   { timestamps: true }

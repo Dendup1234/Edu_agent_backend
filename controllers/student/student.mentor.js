71069abf575a76f15e6687fe0083d8fc46ad9751
Dendup1234 <dendup091@gmail.com>
2026-02-03 16:31:00 +0600

    Fixed mentor api

--- controllers/student/student.mentor.js
+++ controllers/student/student.mentor.js
@@ -39,7 +39,7 @@ export const connectMentor = async (req, res) => {
 
     // Add mentee only if this student isn't already in mentees array
     const mentor = await Mentor.findOneAndUpdate(
-      { _id: mentorId, "mentees.student": { $ne: userId } },
+      { _id: mentorId },
       { $push: { mentees: { student: userId, status: "pending" } } },
       { new: true, runValidators: true },
     );

36135fb9cc9f607ecc32cdd740df4e039a529a72
Dendup1234 <dendup091@gmail.com>
2026-02-04 11:08:05 +0600

    removed rate limiting for testing

--- server.js
+++ server.js
@@ -18,15 +18,16 @@ import rateLimit from "express-rate-limit";
 dotenv.config();
 
 // rate limiting
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100,
-});
+
+//const limiter = rateLimit({
+//windowMs: 15 * 60 * 1000, // 15 minutes
+//max: 100,
+//});
 // Express app command
 const app = express();
 
 // rate limiting
-app.use(limiter);
+//app.use(limiter);
 //helmet config
 app.use(helmet());
 

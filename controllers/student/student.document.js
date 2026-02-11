4b3ea9b5edf00fabb02c450b7bd3a370a2d26f64
yumiko-senpai <damcheynorbu7739@gmail.com>
2026-02-11 15:24:34 +0600

    UPDATE: student document fetch logic

--- controllers/student/student.document.js
+++ controllers/student/student.document.js
@@ -50,6 +50,7 @@ export const getDocuments = async (req, res) => {
     const documents = await Document.find({
       belongsTo: studentId,
       agency: student.registeredAgency,
+      documentCategory: null
     }).lean();
 
     return res.status(200).json({ data: documents });

b0f12c1ab94ff5916aa417f8a84cde595c236788
Dendup1234 <dendup091@gmail.com>
2026-02-13 22:36:32 +0600

    fixed socket

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.use("/api/v1/openai", chatRoutes);
 // Server setups
 const PORT = process.env.PORT || 8000;
 
-// Create HTTP server from Express
+// Create HTTP server from Expresses
 const server = http.createServer(app);
 
 // Initialize Socket.IO
